3335006b9978161754a4764d318a787ac3d13838
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2023-11-05 14:56:02 +0530

    added login and signup in nav

--- Frontend-react/pisb-todo-app/src/Navbar.js
+++ Frontend-react/pisb-todo-app/src/Navbar.js
@@ -1,18 +1,15 @@
 const Navbar = () => {
-  return ( 
-      <nav className="navbar">
-          <h1>Todos List</h1>
-          <div className="links">
-      <a href="/">Home</a>
-      <a href="/create" style={{ 
-        color: 'white', 
-        backgroundColor: '#f1356d',
-        borderRadius: '8px' 
-      }}>Tasks</a>
-    </div>
+  return (
+    <nav className="navbar">
+      <h1>Todos List</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/login">Login</a>
+        <a href="/signup">Signup</a>
+      </div>
+    </nav>
+  );
+};
 
-      </nav>
-   );
-}
 
 export default Navbar;
---
+++ Frontend-react/pisb-todo-app/src/Signup.js
@@ -0,0 +1,13 @@
+// Signup.js
+import React from 'react';
+
+const Signup = () => {
+  // Implement your signup form here
+  return (
+    <div>
+      {/* Signup form */}
+    </div>
+  );
+};
+
+export default Signup;
