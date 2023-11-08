d9c2a41a958fd70a43477feef78ca21d988542a5
Prasad <prasad.gujar2004@gmail.com>
2023-11-09 01:40:06 +0530

    given name as todotrac

--- Frontend-react/pisb-todo-app/src/Navbar.js
+++ Frontend-react/pisb-todo-app/src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Todos List</h1>
+      <h1>TodoTrac</h1>
       <div className="links">
         {/* <a href="/">Home</a> */}
         <a href="/">Logout</a>
--- Frontend-react/pisb-todo-app/src/Navbar1.js
+++ Frontend-react/pisb-todo-app/src/Navbar1.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
       <nav className="navbar1">
-        <h1>Todos List</h1>
+        <h1>TodoTrac</h1>
         <div className="links">
           <a href="/">Home</a>
           <a href="/login">Login</a>
